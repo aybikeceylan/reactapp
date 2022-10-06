2e970e10d662e846f3d959c082fb162071606e16
aybike <aybikesayar@gmail.com>
2022-10-06 12:18:12 +0300

    cmmt

--- src/Counter.js
+++ src/Counter.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 
 const Counter = () => {
     const [count, setCount] = useState(0)
-        (
-            <div>
-                <h4>Counter: {count}</h4>
-                <button onClick={() => setCount(count++)}>➕</button>
-                <button onClick={() => setCount(count--)}>➖</button>
-            </div>
-        )
+    return (
+        <div>
+            <h4>Counter: {count}</h4>
+            <button onClick={() => setCount(count + 1)}>➕</button>
+            <button onClick={() =>
+                counter > 0 ? setCount(count - 1) : counter == 0}>➖</button>
+        </div>
+    )
 }
 
 export default Counter
